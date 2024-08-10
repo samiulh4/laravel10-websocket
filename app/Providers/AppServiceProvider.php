0e7a1dc1a123bbbe95fe1b7b4d7b8bc0a80e2103
Md.Sami Ul Haque <samiulh4@gmail.com>
2024-08-10 23:20:05 +0600

    Init Commit
    Init Commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // Set a default string length to avoid the specified key length error
+        Schema::defaultStringLength(191);
+    }
+}
